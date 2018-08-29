1afa8d69cbddf2046bc6953a6ba2fdbe84cc5e6c
God of the Big Maccys <36157416+AmberWilliamson@users.noreply.github.com>
2018-08-29 12:19:21 +1000

    Algorithm Design
    She ain't done tho

---
+++ Algorithm Design Thingoooooooo.py
@@ -0,0 +1,20 @@
+__author__ = "Amber Williamson"
+__licence__ = "GPL"
+__version__ = "1.0.1"
+__email__ = "amber.williamson@education.nsw.gov.au"
+__status__ = "Production, Prototype or Development"
+
+dumbAssArray = []
+def loadArray():
+    print("loadArray was called")
+
+def printArray():
+    pass
+    
+a = input('Would you like to A - add numbers P - print our the array S - sum the array Q - quit ')
+if a == '1':
+   loadArray(theArray)
+elif a == 'P':
+   printArray()
+elif a == 'S':
+   sumArray()       
